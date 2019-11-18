e6775ada679704c8676d588dd30de7946d980ea2
Sherry Luo <sherryluo08@gmail.com>
2019-11-18 16:22:26 -0500

    sunburst plot edit

--- SunBurst.R
+++ SunBurst.R
@@ -4,7 +4,7 @@ library(stringr)
 library(sunburstR)
 df <- get_cansim(1410006401)
 
-#2000-2018
+#2000-2018 and all of Canada
 #Median hourly wage rate for male/female
 
 #clean data
@@ -24,11 +24,17 @@ df2 <- data.frame((df1
          %>% drop_na()
          %>% rename(value = VALUE))
 
-
- 
-
 #not displaying wages but percentages?
 sunburst(df2,percent = FALSE)
+
+#things to work on if we go forward
+# which years to focus on
+# reorder the columns in the data so it displays sex, year, etc. 
+# what are the main things to focus on
+# results may look better if i used the industry vs. # of ppl in industry
+# this is the industry vs. wage data... (my bad)
+
+
 
 
 
