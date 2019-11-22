659102bc7564e9158d9af22ea083253b6d2120ac
yxwangm <55031660+yxwangm@users.noreply.github.com>
2019-11-21 23:57:22 -0500

    Workind sample plot of Industry by Region by Education by Female Representation

--- Geospatial_Sample.R
+++ Geospatial_Sample.R
@@ -13,8 +13,7 @@ library(viridis)
 library(broom)
 library(gganimate)
 library(RColorBrewer)
-library(tidyr)
-library(dplyr)
+library(plotly)
 
 #Geospatial data related packages
 library(maps)
@@ -26,75 +25,110 @@ library(geogrid)
 library(htmlwidgets)
 library(rgdal) #This seems to work better for me than sf for reading shape files when they need to be re-projected
 
-#Current figure is demographic breakdown by region and industry
+#Current figure is gender breakdown by region and industry
+#Create multiple facet plots in question
+#Interactive element omitted - too difficult.
 
-
-##Download and Read zip file
+##Download and Read zip file into local director
 ## Next step is to redirect it into a neater folder... sorry about that
-## Many, many columns to rename...
-
-
-## Zip File into Local Directory
 download.file("https://www12.statcan.gc.ca/census-recensement/2016/dp-pd/hlt-fst/edu-sco/Tables/Files/98-402-X2016010-T4-csv-eng.zip", destfile="statscan.zip")
 
-## Read CSV (Note: Parsing failures are blank rows in the data)
-## Put Fields of Study as Identifiers next to education level in a single column
-## Collect Industry counts and Industry Percentages into one column each with labels
-source <- (read_csv(unzip("statscan.zip", "98-402-X2016010-T4-CANPR-eng.csv")) 
-           %>% rename("Freq"="Total – STEM and BHASE (non-STEM) groupings - Classification of Instructional Programs (CIP) 2016 (2016 counts)",
-                      "SciTech"="STEM fields of study - Science and science technology (2016 counts)",
-                      "EngTech"="STEM fields of study - Engineering and engineering technology (2016 counts)",
-                      "MathCS"="STEM fields of study - Mathematics and computer and information science (2016 counts)",
-                      "BizAdmin"="BHASE (non-STEM) fields of study - Business and administration (2016 counts)",
-                      "Artsy"="BHASE (non-STEM) fields of study - Arts and humanities (2016 counts)",
-                      "SocSci"="BHASE (non-STEM) fields of study - Social and behavioural sciences (2016 counts)",
-                      "Legal"="BHASE (non-STEM) fields of study - Legal professions and studies (2016 counts)",
-                      "Health"="BHASE (non-STEM) fields of study - Health care (2016 counts)",
-                      "EdTeach"="BHASE (non-STEM) fields of study - Education and teaching (2016 counts)",
-                      "Trade"="BHASE (non-STEM) fields of study - Trades, services, natural resources and conservation (2016 counts)",
-                      "TotalPercent"="Total – STEM and BHASE (non-STEM) groupings - Classification of Instructional Programs (CIP) 2016 (% distribution 2016)"
-           )
-           %>% drop_na()
-           %>% gather(c(9:18), key="Industry", value="Counts") 
-           %>% gather(c(10:19), key="IndustryPercent", value="Percent")
-           %>% dplyr::select(-c("Geographic code", "Global non-response rate", "Data quality flag", "TotalPercent", "IndustryPercent"))
+## Unzip and read CSV (Note: Parsing failures are blank rows in the data)
+## Many, many columns to rename...
+## Drop useless columns and NA rows
+source <- read_csv(unzip("statscan.zip", "98-402-X2016010-T4-CANPR-eng.csv")) 
+source_renamed <- (source %>% rename("GeoName"="Geographic name", 
+                                   "AreaTotal"="Total – STEM and BHASE (non-STEM) groupings - Classification of Instructional Programs (CIP) 2016 (2016 counts)",
+                                   "SciTech"="STEM fields of study - Science and science technology (2016 counts)",
+                                   "EngTech"="STEM fields of study - Engineering and engineering technology (2016 counts)",
+                                   "MathCS"="STEM fields of study - Mathematics and computer and information science (2016 counts)",
+                                   "BizAdmin"="BHASE (non-STEM) fields of study - Business and administration (2016 counts)",
+                                   "Artsy"="BHASE (non-STEM) fields of study - Arts and humanities (2016 counts)",
+                                   "SocSci"="BHASE (non-STEM) fields of study - Social and behavioural sciences (2016 counts)",
+                                   "Legal"="BHASE (non-STEM) fields of study - Legal professions and studies (2016 counts)",
+                                   "Health"="BHASE (non-STEM) fields of study - Health care (2016 counts)",
+                                   "EdTeach"="BHASE (non-STEM) fields of study - Education and teaching (2016 counts)",
+                                   "Trade"="BHASE (non-STEM) fields of study - Trades, services, natural resources and conservation (2016 counts)",
+                                   "TotalPercent"="Total – STEM and BHASE (non-STEM) groupings - Classification of Instructional Programs (CIP) 2016 (% distribution 2016)"
+)
+%>% drop_na()
+%>% dplyr::select(-c(19:29)))
+
+###############################################
+
+#There may be a way to simplify this...
+
+##Numbers for both genders
+bothsexes <- (source_renamed %>% gather(c(9:18), key="Industry", value="TotalGenderCount") 
+%>% filter(GeoName!="Canada")
+%>% filter(Age=="All ages, 15-plus")
+%>% filter(Sex== "Both sexes")
+%>% dplyr::select(-c("Geographic code", "Sex", "Global non-response rate", "Data quality flag"))
 )
 
+##Numbers for females only
+females <- (source_renamed %>% gather(c(9:18), key="Industry", value="FemaleGenderCount") 
+%>% filter(GeoName!="Canada")
+%>% filter(Age=="All ages, 15-plus")
+%>% filter(Sex== "Female")
+%>% dplyr::select(-c("Geographic code", "Sex", "AreaTotal", "Global non-response rate", "Data quality flag"))
+)
 
-## Load Shape File
+##Recombine Tables and Calculate Female Proportions
+##Filter out Most Frequent Industries per Jurastiction
+femaleprop <- (left_join(bothsexes,females) 
+               %>% mutate(FemalePercent = 100*(FemaleGenderCount/TotalGenderCount))
+)
 
-canada_shape <- readOGR("Shape Files/lpr_000b16a_e.shp")
+####################################################
 
-#Project shape file to latitude/longitude (scaling for map overlay if needed)
-p3 <- spTransform(canada_shape, CRS("+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs")) 
+##Create Five Tables For Different Education Levels
+##Within each table, extract top value for each province
+##Not ideal but this level of striation requires some drastic measures to clean
 
-#Force transformed shape file into data frame so it can be put over the map as geom_map
-#Will generate some warning messages if warnings are not suppressed, which don't seem to be relevant, 
-#Don't think there is a better way to handle this
-tidydta <- tidy(p3, group=group)
+College <- (femaleprop %>% filter(Education=="College, CEGEP or other non-university certificate or diploma") 
+            %>% group_by(GeoName) %>% slice(which.max(TotalGenderCount)))
+UniCert <- (femaleprop %>% filter(Education=="University certificate, diploma or degree at bachelor level or above")
+            %>% group_by(GeoName) %>% slice(which.max(TotalGenderCount)))
+Bach <- (femaleprop %>% filter(Education=="Bachelor's degree")
+         %>% group_by(GeoName) %>% slice(which.max(TotalGenderCount)))
+Master <- (femaleprop %>% filter(Education=="Master's degree")
+           %>% group_by(GeoName) %>% slice(which.max(TotalGenderCount)))
+PhD <- (femaleprop %>% filter(Education=="Earned doctorate")
+        %>% group_by(GeoName) %>% slice(which.max(TotalGenderCount)))
+ 
+dtafin <- bind_rows(College, UniCert, Bach, Master, PhD, id=NULL)
+ 
+## Load Shape File, Join Shape File with Data (it works reasonably now even on my laptop)
+## Major change is the shape file used - less precise but reduces render time to seconds from minutes
+canada_shape <- st_read("Shape Files/Digital Boundary/lpr_000a16a_e.shp")
+joined_dta <- left_join(canada_shape, dtafin, by=c("PRENAME"="GeoName"))
 
-buffer <- 0.01        ## Add a small lat/long buffer to the edges of the map layer to make sure shape files aren't touching the edge
-darkness_value <- 0.25 ## Transparency setting for scatter points
-tint <- 0.4           ## Tinting for watercolor map and shape files (make things a little less vibrant so the data shows up better)
+
+##SciTech_Male
 
 #Changed plot, forgot to update this part
 #Will fill in sometime tomorrow
-(ggplot(source)
-  + geom_sf(aes(fill=),alpha=tint, map=tidydta, color="#404040")
-+ scale_fill_viridis(name = "Industry", discrete=TRUE, option="plasma")
-+ xlab("Longitude")
-+ ylab("Latitude")
-+ facet_wrap(~room_type, nrow=2, ncol=2)
-+ guides(alpha=FALSE)
-+ theme(legend.position=c(0.75, 0.25), 
-        panel.spacing = unit(0.5, "lines"),
-        axis.text = element_text(size=12),
-        legend.key.size = unit(0.5, "in"),
-        legend.title = element_text(size=20),
-        legend.text = element_text(size=18),
-        axis.title.x = element_text(size=18),
-        axis.title.y = element_text(size=18),
-        strip.text.x = element_text(size=20),)
+
+#p3 <- spTransform(canada_shape, CRS("+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs")) 
+#tidydta <- tidy(p3, group=group)
+
+(ggplot(joined_dta)
+  + geom_sf(aes(fill=Industry, geometry=geometry, alpha=FemalePercent))
+  + scale_fill_viridis(name = "Industry", discrete=TRUE, option="plasma")
+  + scale_alpha_continuous(name = "Percent Female", range=c(0.5, 1))
+  + xlab("Longitude")
+  + ylab("Latitude")
+  + facet_wrap(~Education)
+  + theme_classic()
+  + theme(legend.position=c(0.83, 0.25), 
+          panel.spacing = unit(0.5, "lines"),
+          axis.text = element_text(size=12),
+          legend.title = element_text(size=10),
+          legend.text = element_text(size=10),
+          axis.title.x = element_text(size=10),
+          axis.title.y = element_text(size=10),
+          strip.text.x = element_text(size=10),)
 )
 
 ## Next steps:
